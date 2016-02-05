3f53eefb96325b6658a405053a3dc62f8d55f663
Vivian Vriezekolk <vivianvriezekolk@gmail.com>
2016-02-05 13:21:39 +0100

    bijna af

--- Assignment1/src/Groep.java
+++ Assignment1/src/Groep.java
@@ -1,18 +1,51 @@
 public class Groep {
 	private int aantal;
+	private Student[] studenten;
+	private int size;
+	private int index;
 
 	public Groep() {
 
 	}
 
 	public Groep(int aantal) {
+		index = 0;
+		size = 0;
 		this.aantal = aantal;
-		Student [] studenten = new Student [aantal]; // dit weet ik niet zeker hoor, waar we dit object aan moeten maken..
+		studenten = new Student[aantal]; // dit weet ik niet zeker hoor, waar we
+											// dit object aan moeten maken..
 
 	}
-	
-	public boolean voegToe(Student s){
-		return true;
+
+	public boolean voegToe(Student s) {
+		if (size == aantal) {
+			return false;
+		} else {
+			studenten[size] = s;
+			size += 1;
+			return true;
+		}
 	}
 
+	public Student getStudent(int i) {
+		//size = studenten.length;
+		if (size < i) {
+			return null;
+		} else {
+			return studenten[i];
+		}
+
+	}
+
+	public String toString() {
+		if (index == size) {
+			return " ";
+		} else {
+			index += 1;
+			return studenten[index - 1] + "\n" + toString();
+		}
+
+		// for(int i = 0; i < studenten.length; i++)
+		// }
+	}
 }
--- Assignment1/src/Main.java
+++ Assignment1/src/Main.java
@@ -1,13 +1,43 @@
+import java.util.Scanner;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Student bob = new Student("Bob", "de Vries", 7);
-		System.out.println("De student: " + bob);
+		System.out.println("Hoeveel studenten mogen er in de groep?");
+		Scanner scanner = new Scanner(System.in);
+		int aantal = scanner.nextInt();
+		
+		Groep groep = new Groep(aantal);
+		
+		for(int i = 0; i < aantal; i++) {
+			System.out.println()
+			String naam = scanner.nextLine();
+			
+		}
+		/*Student bob = new Student("Bob", "de Vries", 7);
+		Student hester = new Student("Hester", "Huijsdens", 4480562);
+		Student vivian = new Student("Vivian", "Vriezekolk", 4482921);
+		
+		Groep groep = new Groep(3);
+		Groep groep2 = new Groep(5);
+		
+	//	System.out.println("De student: " + bob);
 		bob.setNaam("Bob" , "De Bouwer") ;
+		
 		System.out.println("De student: " + bob);
 		
+		groep.voegToe(hester);
+		groep.voegToe(vivian);
+		groep2.voegToe(vivian);
+		groep2.voegToe(bob);
+		System.out.println(groep.getStudent(0));
+	//	System.out.println(groep.getStudent(1));
+		System.out.println(groep2.getStudent(0));
+	//	System.out.println(groep2.getStudent(2));
+		
+		System.out.println(groep);
+		System.out.println(groep2); */
 	}
 
 }
